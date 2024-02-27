5a196114a2ef03dfb643c28d4aa48ab766b33620
Jules <juleslordet@gmail.com>
2024-02-27 12:33:39 +0100

    add shop section nav bar

--- app.js
+++ app.js
@@ -5,13 +5,37 @@ function callApi() {
     .then(response => {
         const data = response.data
         console.log(data)
-        const desc_container = document.getElementById('desc_container');
-        const desc = data[0].description
-        desc_container.textContent = desc
+        item_shop_section(data)
     })
     .catch(error => {
         console.error('Erreur lors de la requête GET', error);
     });
+}
+
+function item_shop_section(data) {
+        const container_name_id_1 = document.getElementById(`shop_sec_id_1`);
+        const name_id_1 = data[0].name;
+        container_name_id_1.textContent = name_id_1;
+
+        const container_name_id_2 = document.getElementById(`shop_sec_id_2`);
+        const name_id_2 = data[1].name;
+        container_name_id_2.textContent = name_id_2;
+
+        const container_name_id_3 = document.getElementById(`shop_sec_id_3`);
+        const name_id_3 = data[2].name;
+        container_name_id_3.textContent = name_id_3;
+
+        const container_name_id_4 = document.getElementById(`shop_sec_id_4`);
+        const name_id_4 = data[3].name;
+        container_name_id_4.textContent = name_id_4;
+
+        const container_name_id_5 = document.getElementById(`shop_sec_id_5`);
+        const name_id_5 = data[4].name;
+        container_name_id_5.textContent = name_id_5;
+
+        const container_name_id_6 = document.getElementById(`shop_sec_id_6`);
+        const name_id_6 = data[5].name;
+        container_name_id_6.textContent = name_id_6;
 }
 
 callApi()
