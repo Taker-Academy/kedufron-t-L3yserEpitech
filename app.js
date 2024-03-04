b5acf108942da75cc1a06a09077aaf713cac1ec4
Jules <juleslordet@gmail.com>
2024-03-04 08:35:42 +0100

    ADD body -> popular item section

--- app.js
+++ app.js
@@ -13,52 +13,25 @@ function callApi() {
 }
 
 function item_shop_section(data) {
-        const container_name_id_1 = document.getElementById(`shop_sec_id_1`);
-        const name_id_1 = data[0].name;
-        container_name_id_1.textContent = name_id_1;
+        const container_main_product = document.getElementById(`name_main_product`)
+        const name = data[1].name
+        container_main_product.textContent = name + ' !'
 
-        const container_name_id_2 = document.getElementById(`shop_sec_id_2`);
-        const name_id_2 = data[1].name;
-        container_name_id_2.textContent = name_id_2;
+        const container_desc_mp = document.getElementById('desc_mp')
+        const desc = data[1].description
+        container_desc_mp.textContent = desc
 
-        const container_name_id_3 = document.getElementById(`shop_sec_id_3`);
-        const name_id_3 = data[2].name;
-        container_name_id_3.textContent = name_id_3;
+        const container_bt_mp = document.getElementById('bt_mp')
+        const bt_mp = data[1].name
+        container_bt_mp.textContent = 'Shop ' + bt_mp
+        container_bt_mp.classList.add('center-content');
 
-        const container_name_id_4 = document.getElementById(`shop_sec_id_4`);
-        const name_id_4 = data[3].name;
-        container_name_id_4.textContent = name_id_4;
-
-        const container_name_id_5 = document.getElementById(`shop_sec_id_5`);
-        const name_id_5 = data[4].name;
-        container_name_id_5.textContent = name_id_5;
-
-        const container_name_id_6 = document.getElementById(`shop_sec_id_6`);
-        const name_id_6 = data[5].name;
-        container_name_id_6.textContent = name_id_6;
-
-        const imageContainer = document.getElementById('popular_img');
-        const imageUrl = 'https://api.kedufront.juniortaker.com/item/picture/1';
+        const imageContainer = document.getElementById('photo_mp');
+        const imageUrl = 'https://api.kedufront.juniortaker.com/item/picture/2';
         const imageElement = document.createElement('img');
         imageElement.src = imageUrl;
-        imageElement.style.width = '300px'
+        imageElement.style.width = '550px'
         imageContainer.appendChild(imageElement);
-
-
-        const logoElement = document.querySelector('.shop_button')
-        const shopDiv = document.querySelector('.shop_div')
-
-        // Ajout d'un écouteur d'événements pour le survol de la souris sur l'élément .logo
-        logoElement.addEventListener('mouseenter', function() {
-        // Au survol de .logo, changer le style de .shop_div
-        shopDiv.style.display = 'flex'
-        })
-
-        // Ajout d'un écouteur d'événements pour la sortie de la souris de l'élément .logo
-        logoElement.addEventListener('mouseleave', function() {
-        // Quand la souris quitte .logo, masquer .shop_div
-        shopDiv.style.display = 'none'
-        })
 }
 
 
